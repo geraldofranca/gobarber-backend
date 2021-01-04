7c572ddaa7b75159caedba0a5407af269327ef28
Geraldo França <geraldo.pereira.franca@gmail.com>
2021-01-04 13:59:03 -0300

    refactor: modularizing the code

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,14 +1,22 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import { getRepository } from 'typeorm';
+import uploadConfig from '@config/upload';
 
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import User from '../../typeorm/entities/User';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
-
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+
+usersRouter.get('/', async (request, response) => {
+  const usersRepository = getRepository(User);
+  const users = await usersRepository.find();
+
+  return response.json(users);
+});
 
 usersRouter.post('/', async (request, response) => {
   try {
